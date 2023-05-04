1f95102b21f813a01f8a24ad770f0f1cb6fb42d8
michelepennisi <michele.pennisi@cern.ch>
2023-05-04 16:02:14 +0200

    removing selection on repeted muons

---
+++ mc_analysis/grid_task/powheg_sim_analysis/AddTaskDimuonHF.C
@@ -0,0 +1,32 @@
+AliAnalysisTaskDimuonHF *AddTaskDimuonHF(TString MC_type, Int_t RunNumber = 294009)
+{
+
+   //****************************************************************************************
+   // Add task class.
+   // The attached class prepares a MC tree (embedding) with generated and reconstructed muons/dimuons
+   // Michele Pennisi
+   //****************************************************************************************
+
+   printf("Creating Task to creat a Muon/Dimuon tree from Embedding\n");
+
+   AliAnalysisManager *mgr = AliAnalysisManager::GetAnalysisManager();
+   if (!mgr)
+   {
+      ::Error("AddTaskDimuonHF", "No analysis manager to connect to.");
+      return NULL;
+   }
+   TString fnameout;
+   fnameout.Form("Powheg_%sSim_MCDimuHFTree_%d.root", MC_type.Data(), RunNumber);
+   printf("Fnameout = %s\n", fnameout.Data());
+
+   AliAnalysisDataContainer *coutput1 = mgr->CreateContainer("MCTree", TTree::Class(), AliAnalysisManager::kOutputContainer, fnameout);
+
+   AliAnalysisTaskDimuonHF *MCQuarkoniumTask = new AliAnalysisTaskDimuonHF("AliAnalysisTaskDimuHFTreeMC");
+
+   mgr->AddTask(MCQuarkoniumTask);
+
+   mgr->ConnectInput(MCQuarkoniumTask, 0, mgr->GetCommonInputContainer());
+   mgr->ConnectOutput(MCQuarkoniumTask, 1, coutput1);
+
+   return MCQuarkoniumTask;
+}
